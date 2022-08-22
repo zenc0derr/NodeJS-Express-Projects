9073a72c84a1fe512ee7e263a24ea7adc8a133fb
zenc0derr <119teja@gmail.com>
2022-08-22 22:04:21 +0530

    completed JWT Basics Project

---
+++ JWT Basics/controllers/main.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+
+const login = async (req,res) => {
+    const {username, password} = req.body
+    if(!username || !password){
+        throw new CustomAPIError('Please provide email and password',400)
+    }
+
+    const id = new Date().getDate()
+
+    const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn : '30d'})
+    //console.log(username,password)
+    res.status(200).json({msg:'User Created',token})    
+}
+
+
+const dashboard = async (req, res) => {
+
+    const luckyNumber = Math.floor(Math.random()*100)
+    res
+        .status(200)
+        .json({msg:`Hello, ${req.user.username}`,secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
+}
+
+module.exports = {
+    login,
+    dashboard
+}
